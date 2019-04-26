2ebe40a8eba8bd49426aad8ff77e9e48405ca6b9
LeiDeMing <136371773@qq.com>
2019-04-26 18:11:07 +0800

    fix

--- utils/front2.js
+++ utils/front2.js
@@ -1,18 +1,22 @@
 const puppeteer = require('puppeteer');
 const config = require('../config/index');
 const fs = require('fs');
+const open = require('open');
 
 async function setPupper(url) {
     let browser = await puppeteer.launch({
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     let newPage = await browser.newPage();
+    let imgName=`translation${new Date().getTime()}`
     await newPage.goto(url);
     deleteall('./utils/img')
     fs.mkdirSync('./utils/img', (err) => {
         if (err) throw err;
     })
-    await newPage.screenshot({ path: `./utils/img/translation${new Date().getTime()}.png` })
+    await newPage.screenshot({ path: `./utils/img/${imgName}.png` })
+    // await open(`./utils/img/${imgName}.png`, {wait: true});
+
     // await newPage.type('#userName','wangzhengyang',{
     //     delay:100
     // });
