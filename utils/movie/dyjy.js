4aac5a5ef4ef854a4d19aeed972ad380b91c9b07
LeiDeMing <136371773@qq.com>
2019-05-23 17:32:14 +0800

    add

--- utils/movie/dyjy.js
+++ utils/movie/dyjy.js
@@ -33,9 +33,9 @@ async function getMovieFromDyjy() {
         key: 'textdesc'
     },
     {
-        type:'single',
-        value:'.pic img',
-        key:'imgSrc'
+        type: 'single',
+        value: '.pic img',
+        key: 'imgSrc'
     }]
     let dyjyUrl = `http://www.idyjy.com/w.asp?p=${pageSize}&f=3&n=${maxYear}&l=s`;
     try {
@@ -46,6 +46,25 @@ async function getMovieFromDyjy() {
     const pageNav = await page.$eval('#pages', el => el.innerText);
     pageAll = pageNav.split(':')[1].split('页首页')[0].split('/')[1];
 
+    // let aList = await page.$$('.play-img');
+    // for (let x = 0; x < aList.length; x++) {
+    //     let jsHandleObj = await aList[x].getProperty('href');
+    //     const { value } = jsHandleObj._remoteObject;
+    //     hrefList.push(value)
+    // }
+    // await deepMovie({
+    //     hrefList, page, movieData
+    // })
+
+    await deepPage({
+        page, hrefList, movieData, pageSize, pageAll, maxYear
+    })
+}
+
+async function deepPage(deepPageOpt) {
+    let { page, hrefList, movieData, pageSize, pageAll, maxYear } = deepPageOpt
+    let dyjyUrl = `http://www.idyjy.com/w.asp?p=${pageSize}&f=3&n=${maxYear}&l=s`;
+    await page.goto(dyjyUrl)
     let aList = await page.$$('.play-img');
     for (let x = 0; x < aList.length; x++) {
         let jsHandleObj = await aList[x].getProperty('href');
@@ -53,21 +72,27 @@ async function getMovieFromDyjy() {
         hrefList.push(value)
     }
     await deepMovie({
-        hrefList, page, movieData
+        hrefList, page, movieData,maxYear
     })
-
+    pageSize++
+    if (pageSize <= pageAll) {
+        await deepPage(deepPageOpt);
+    }
 }
 
 async function deepMovie(options) {
-    let { hrefList, page, movieData } = options
+    let { hrefList, page, movieData,maxYear } = options
     await page.goto(hrefList[0]);
     let name = await page.$eval('#name', el => el.innerText);
     let textdesc = await page.$eval('.textdesc', el => el.innerText);
-    let imgSrc = await page.$eval('.pic img', el => el.src);
+    let imgSrc = await page.$eval('.pic img', el => el.src);//down_list
+    let downSrc = await page.$eval('input[name="down_url_list_0"]', el => el.value);
     movieData.push({
         name,
         textdesc,
-        imgSrc
+        imgSrc,
+        year:maxYear,
+        thunder:downSrc
     });
     hrefList.shift();
     if (!!hrefList.length) {
