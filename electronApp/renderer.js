5afc3e12df804ee861c14222e461def8bbf531e8
LeiDeMing <136371773@qq.com>
2019-09-15 10:56:58 +0800

    add

--- electronApp/renderer.js
+++ electronApp/renderer.js
@@ -1,3 +1,6 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const {ipcRenderer} = require('electron')
+const contextMenuBtn = document.getElementById('context-menu')
+
+contextMenuBtn.addEventListener('click', () => {
+  ipcRenderer.send('show-context-menu')
+})
