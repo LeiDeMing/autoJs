06ad9192e2a1fc153cb3b7f7c1ecf0cdcc6814e4
NeiDeMing <xuan136371773@gmail.com>
2020-05-22 10:47:55 +0800

    add

--- electronChanDao/renderer.js
+++ electronChanDao/renderer.js
@@ -268,8 +268,8 @@ btnCloseDom.addEventListener('click', async (e) => {
 scheduleBtn.addEventListener('click', async () => {
     let rule = new schedule.RecurrenceRule();
     let _d = new Date()
-    rule.minute = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60]
-    // rule.minute = [0, 15, 30, 45];
+    // rule.minute = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60]
+    rule.minute = [0, 15, 30, 45];
     scheduleBtn.setAttribute('disabled', 'true')
     setSchedule(rule, async () => {
         console.log('定时任务启动 --- ' + _d)
